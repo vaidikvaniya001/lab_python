113ade486980e68f2f8fe5f2ad36c56251f68921
vaidikvaniya001 <vaidikvaniya001@gmail.com>
2026-01-05 11:17:26 +0530

    Add files via upload

---
+++ vaidik2.py
@@ -0,0 +1,32 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x="hello"
+print(x)
+hello
+x=10
+print(x)
+10
+x=True
+y=False
+print(x<y)
+False
+x=("apple","banana","cherry")
+x=("apple","banana","cherry")
+print(x)
+('apple', 'banana', 'cherry')
+x=["apple","banana","cherry"]
+print(x)
+['apple', 'banana', 'cherry']
+x={"name":"john","age":36}
+print(x)
+{'name': 'john', 'age': 36}
+x=20.5
+print(x)
+20.5
+a=
+SyntaxError: invalid syntax
+print(a)
+Traceback (most recent call last):
+  File "<pyshell#17>", line 1, in <module>
+    print(a)
+NameError: name 'a' is not defined

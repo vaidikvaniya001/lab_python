07894f5b9903efcb099d7e278ce09568f586868d
vaidikvaniya001 <vaidikvaniya001@gmail.com>
2026-01-07 07:57:25 +0530

    Add files via upload

---
+++ one.py
@@ -0,0 +1,43 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+x = 10
+print(x)
+10
+a = 5
+print(-a)
+-5
+p = 10
+q = 5
+print(p>q)
+True
+print(p==q)
+False
+x = True
+y = False
+print
+<built-in function print>
+(
+print(a and y)
+False
+print(x or y)
+True
+print(not x)
+False
+a =5
+b=3
+print(a&b)
+1
+print(a|b)
+7
+number = [1,2,3,4,6]
+print(2 in number) #o/p = true
+True
+print(5 not in number) #o/p = false
+True
+y=20
+y=20
+x=30
+print(x is y)
+False
+print(x is not y)
+True
